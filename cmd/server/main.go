e952054cab1805671c90509384cb1bdbf3859438
Hayden Hargreaves <hhargreaves2006@gmail.com>
2025-02-25 20:04:51 -0700

    FIX: Using port 3005

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -9,7 +9,7 @@ import (
 
 // TODO: Implement port backtesting. When when fails, try the next one until we get a open port.
 func main() {
-	s := server.NewTCPServer(server.WithPort(3000), server.WithTLS(), server.WithMaxConn(2))
+	s := server.NewTCPServer(server.WithPort(3005), server.WithTLS(), server.WithMaxConn(2))
 	ln := s.Configure("./certs/server.crt", "./certs/server.key").Listen()
 	for _, err := range s.Errors {
 		panic(err)
